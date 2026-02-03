98465737bcaceec2f9ef0bc346a5a228383193c9
jotao22 <jotao22gm@gmail.com>
2026-02-03 20:13:10 -0300

    dot_env corrigido

--- Backend/backend/wsgi.py
+++ Backend/backend/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+from json import load
+from dotenv import load_dotenv
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
+load_dotenv()
 application = get_wsgi_application()
--- Backend/manage.py
+++ Backend/manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from json import load
 import os
 import sys
+
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    load_dotenv()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
